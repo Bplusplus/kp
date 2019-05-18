949984430b52188be147f7bf64cd5614a6b88646
CaptNemo <jironomics2@gmail.com>
2019-05-17 20:23:14 -0700

    this is the most recent change

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -10,39 +10,68 @@ public class PlayerInput : MonoBehaviour {
     public float yMax = 4;
     public float yMin = -4;
 
-	public serialMicrobitInput Microbit = null;
+    public string Axis1 = "Fire1";
+    public string Axis2 = "Fire2";
+
+    public serialMicrobitInput Microbit = null;
+
+    public bool useButtons = true;
 
     void Update() {
 
-		if (Microbit == null)
-		{
+        if (useButtons)
+        {
+            float input2 = 0f;
 
-			float input = 0f;
+            if (Input.GetAxis(Axis1) > 0)
+            {
+                input2 = 1;
+                Debug.Log(Input.GetAxis(Axis1).ToString());
+            }
+            else if (Input.GetAxis(Axis2) > 0)
+            {
+                input2 = -1;
+                Debug.Log(Input.GetAxis(Axis2).ToString());
+            }
 
-			if (isPlayerOne)
-			{
-				input = Input.GetAxisRaw("Vertical");
-			}
-			else
-			{
-				input = Input.GetAxisRaw("Vertical2");
-			}
+            Vector3 Pos2 = gameObject.transform.position;
+            Pos2.y += speed * input2;
+            Pos2.y = Mathf.Clamp(Pos2.y, yMin, yMax);
+            gameObject.transform.position = Pos2;
+        }
+        else
+        {
 
-			Vector3 Pos = gameObject.transform.position;
-			Pos.y += speed * Time.deltaTime * input;
-			Pos.y = Mathf.Clamp(Pos.y, yMin, yMax);
-			gameObject.transform.position = Pos;
+            if (Microbit == null)
+            {
+
+                float input = 0f;
+
+                if (isPlayerOne)
+                {
+                    input = Input.GetAxisRaw("Vertical");
+                }
+                else
+                {
+                    input = Input.GetAxisRaw("Vertical2");
+                }
+
+                Vector3 Pos = gameObject.transform.position;
+                Pos.y += speed * Time.deltaTime * input;
+                Pos.y = Mathf.Clamp(Pos.y, yMin, yMax);
+                gameObject.transform.position = Pos;
 
 
 
-		}
-		else
-		{
-			Vector3 Pos = gameObject.transform.position;
-			Pos.y = Microbit.outputF * yMax;
-			gameObject.transform.position = Pos;
-		}
-
+            }
+            else
+            {
+                Vector3 Pos = gameObject.transform.position;
+                Pos.y = Microbit.outputF * yMax;
+                gameObject.transform.position = Pos;
+            }
+        }
+        
 
     }
 }
